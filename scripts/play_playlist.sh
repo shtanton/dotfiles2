d5e47e6e9ad7afd2223e994e63cfae8a02cee7bb
Charlie Stanton <charlie@shtanton.com>
2020-12-11 19:10:43 +0000

    Moves more stuff to scripts

---
+++ scripts/play_playlist.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/sh
+echo '{"command": ["quit"]}' | socat - /tmp/mpvsocket ;
+mpv --input-ipc-server=/tmp/mpvsocket --loop-playlist --no-audio-display --playlist=$(fd -t f --max-depth=1 . /mnt/hdd/music | menu.sh)
---
+++ scripts/swaybar_command.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+while :
+do
+    date "+%Y-%m-%d %H-%M-%S"
+    sleep 5
+done
