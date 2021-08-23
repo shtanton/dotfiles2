c5d90361b959708ae32276c6bd74b5d356da8929
Charlie Stanton <charlie@shtanton.com>
2021-08-23 12:00:05 +0100

    Add playing music from clipboard

--- scripts/play_playlist.sh
+++ scripts/play_playlist.sh
@@ -6,20 +6,28 @@ oxygene
 retrowave
 retrowave.one
 electroswing-radio
-drumandbass.fm" | menu.sh)
+drumandbass.fm
+bbc radio one
+paste" | menu.sh)
 case $TARGET in
-    playlist|oxygene|retrowave|new)
-        mpv --input-ipc-server=/tmp/mpvsocket --loop-playlist --no-audio-display --playlist="/home/charlie/music/playlists/$TARGET"
-        ;;
-    retrowave.one)
-        mpv --input-ipc-server=/tmp/mpvsocket --no-audio-display "http://85.234.59.191:8000/stream.m3u"
-        ;;
-    electroswing-radio)
-        mpv --input-ipc-server=/tmp/mpvsocket --no-audio-display "https://streamer.radio.co/s2c3cc784b/listen"
-        ;;
-    drumandbass.fm)
-        mpv --input-ipc-server=/tmp/mpvsocket --no-audio-display "http://radio.drumandbass.fm/listen192.m3u"
-        ;;
-    *)
-        ;;
+	playlist|oxygene|retrowave|new)
+		mpv --input-ipc-server=/tmp/mpvsocket --loop-playlist --no-audio-display --playlist="/home/charlie/music/playlists/$TARGET"
+		;;
+	retrowave.one)
+		mpv --input-ipc-server=/tmp/mpvsocket --no-audio-display "http://85.234.59.191:8000/stream.m3u"
+		;;
+	electroswing-radio)
+		mpv --input-ipc-server=/tmp/mpvsocket --no-audio-display "https://streamer.radio.co/s2c3cc784b/listen"
+		;;
+	drumandbass.fm)
+		mpv --input-ipc-server=/tmp/mpvsocket --no-audio-display "http://radio.drumandbass.fm/listen192.m3u"
+		;;
+	"bbc radio one")
+		mpv --input-ipc-server=/tmp/mpvsocket --no-audio-display ~/data/bbc_radio_1.pls
+		;;
+	"paste")
+		mpv --input-ipc-server=/tmp/mpvsocket --no-audio-display --vid=no $(wl-paste)
+		;;
+	*)
+		;;
 esac
