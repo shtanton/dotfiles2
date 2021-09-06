1486067957250f9752942887e021e17b85cdbec7
Charlie Stanton <charlie@shtanton.xyz>
2021-09-06 09:51:57 +0100

    Add rip_cd for ripping cds

--- scripts/irc_send.sh
+++ scripts/irc_send.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/dash
 P=$(find ~/irc -name "in" | awk '{sub(/^.*\/irc\//, ""); sub(/\/in$/, ""); print $0}' | menu.sh)
 IN="$HOME/irc/${P}/in"
-MSG=$(echo "" | menu.sh)
-echo $MSG > $IN
+cat - > $IN
